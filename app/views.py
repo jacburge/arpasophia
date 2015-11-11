d6096ed4dc3404416ddeeed1a711b853bfe2487b
Jaclyn Burge <jacburge@gmail.com>
2015-11-10 20:57:26 -0800

    Added flask

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/about/')
+def about():
+	return render_template('about.html')
+
+@app.route('/schedule/')
+def schedule():
+	return render_template('schedule.html')
+
+@app.route('/news/')
+def news():
+	return render_template('news.html')
